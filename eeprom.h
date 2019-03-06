4442ba940121e3bea26f33e3a5433e8f6281b096
alisoam <ali.sorouramini@gmail.com>
2019-03-06 09:57:44 +0330

    initial commit

---
+++ eeprom.h
@@ -0,0 +1,18 @@
+#ifndef EEPROM_H
+#define EEPROM_H
+
+# ifndef EEPROM_ADDR
+#   define EEPROM_ADDR 0x50
+# endif
+void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data );
+
+// WARNING: address is a page address, 6-bit end will wrap around
+// also, data can be maximum of about 30 bytes, because the Wire library has a buffer of 32 bytes
+void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte* data, byte length );
+
+byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress );
+
+// maybe let's not read more than 30 or 32 bytes at a time!
+void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *buffer, int length );
+
+#endif
